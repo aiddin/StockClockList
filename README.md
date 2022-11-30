2288f86dac734d7ebbf15f8e4346a2c8b69c1a9e
aiddin <arcticdessert@gmail.com>
2022-11-30 15:02:58 +0800

    init

---
+++ README.md
@@ -0,0 +1,47 @@
+# Clock, main task
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### PROPS
+```
+      glow:'',
+      serverDate: date,
+      simpleTime: boolean,
+      simpleDate: boolean,
+      simpleDateTime: boolean,
+      advancedTime: boolean,
+```
+
+### HOW TO USE
+```
+glow =  for glow fx
+serverdate = pass server time for comparison to client time.
+
+DATETIME FORMAT 
+
+Simple {
+    time
+    date
+    datetime
+}
+Advanced Time :
+
+```
---
+++ src/App.vue
@@ -0,0 +1,84 @@
+<!-- :simpleDate="simpleDate"
+:simpleTime="simpleTime"
+:simpleDateTime="simpleDateTime" -->
+<template>
+<div>
+  <tr>
+    <th>
+      
+       
+        <td><v-text>hehe</v-text></td>
+        <tc-clock 
+        
+    :serverDate="serverDate" 
+    :glow="glow"
+   
+    :simpleTime="simpleTime" 
+    /></th>
+    <th></th>
+    <tc-clock 
+    :serverDate="serverDate" 
+    :glow="glow"
+   
+    :simpleTime="simpleTime" 
+    />
+    <br>
+  </tr>
+  <table>
+    <tc-clock 
+    :serverDate="serverDate" 
+    :glow="glow"
+   
+    :simpleTime="simpleTime" 
+    />
+    <br>
+  
+    <tc-clock 
+    :serverDate="serverDate" 
+    :glow="glow"
+   
+    :advancedTime="advancedTime" 
+    />
+  </table>
+</div>
+ 
+</template>
+<script>
+import TcClock from './components/TcClock.vue'
+const date = new Date();
+export default {
+  name: 'App',
+  components: {
+    TcClock,
+  },
+  data() {
+    return {
+      glow:'',
+      serverDate: date,
+      simpleTime: true,
+      simpleDate: true,
+      simpleDateTime: true,
+      advancedTime: true,
+    };
+  },
+  methods: {
+    setDateTime() {
+      const date = new Date();
+
+      this.serverDate = date;
+      this.glow = '';
+      this.simpleDate = true;
+      this.simpleTime = true;
+      this.simpleDateTime = true;
+      this.advancedTime = true;
+      
+    },
+  },
+
+  mounted() {
+    setInterval(() => {
+      this.setDateTime();
+    }, 0);
+  },
+};
+</script>
