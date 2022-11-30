ab851ef5158a60f275276460c28c6e90afc608bd
aiddin <arcticdessert@gmail.com>
2022-11-30 17:53:05 +0800

    Table list siap

--- src/App.vue
+++ src/App.vue
@@ -3,35 +3,39 @@
 :simpleDateTime="simpleDateTime" -->
 <template>
 <div class="stockbg">
-  <tr>
-    <th>
-    <td><v-text>hehe</v-text></td>
-    <tc-clock 
-    :serverDate="serverDate" 
-    :glow="glow"
-    :simpleTime="simpleTime" 
-    /></th>
-  
-    <tc-clock 
-    :serverDate="serverDate" 
-    :glow="glow"
-    :simpleTime="simpleTime" 
-    />
-    <br>
-  </tr>
   <table>
-    <tc-clock 
-    :serverDate="serverDate" 
-    :glow="glow"
-    :simpleTime="simpleTime" 
-    />
-    <br>
-  
-    <tc-clock 
+  <tr>
+    <th>Company</th>
+    <th>Contact</th>
+    <th>Country</th>
+  </tr>
+  <tr>
+    <td>Alfreds Futterkiste</td>
+    <td>Maria Anders</td>
+    <td>Germany</td>
+  </tr>
+  <tr>
+    <td>Centro comercial Moctezuma</td>
+    <td>Francisco Chang</td>
+    <td>Mexico</td>
+  </tr>
+  <tr>
+    <td v-text="message"></td>
+    <td>Roland Mendel</td>
+    <td>Austria</td>
+  </tr>
+  <tr>
+    <td>Island Trading</td>
+    <td>Helen Bennett</td>
+    <td>UK</td>
+    <td><tc-clock 
     :serverDate="serverDate" 
     :glow="glow"
     :advancedTime="advancedTime" 
-    />
+    /></td>
+  </tr>
+  
+ 
   </table>
 </div>
  
@@ -46,6 +50,7 @@ export default {
   },
   data() {
     return {
+      message: 'Hello this is GeeksforGeeks.',
       glow:'',
       serverDate: date,
       simpleTime: true,
