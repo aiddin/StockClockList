5df56f6f72e031db81aeeb5654c3741dfd30571d
aiddin <arcticdessert@gmail.com>
2022-12-12 17:58:24 +0800

    Update TcStockExch.vue

--- src/components/TcStockExch.vue
+++ src/components/TcStockExch.vue
@@ -1,46 +1,46 @@
 <template>
   <div>
-  <grid
-    ref="grid"
-
-    :style="{ height: '150px' }"
-    :data-items="exchange"
-    :selected-field="selectedField"
-    :columns="columns"
-    @rowclick="onRowClick"
-  ></grid>
-</div>
-  <div>
-    <table class="bg-gray-900 table w-full ">
-        <tr
-          class="border ..."
-          v-for="exch in exch"
-          :key="exch.id"
-        >
-          <td  class="border border-slate-700 ...">
-            <tc-stock-stat :stat="exch.status" />
-          </td>
-          <td class="text-white border border-slate-700 ..." >
-            {{ exch.id }}
-          </td>
-          <td class="text-white border border-slate-700 ...">
-            {{ exch.name }}
-          </td>
-          <td class="text-white border border-slate-700 ...">
-            <tc-clock 
-           
-            :serverDate="exch.serverDate1"
-            :simpleTime="simpleTime"
-            :glow="glow"
-             > 
-          </tc-clock>
-          </td>
-        </tr>
+    <grid
+      ref="grid"
+      :style="{ height: '150px' }"
+      :data-items="exchange"
+      :selected-field="selectedField"
+      :columns="columns"
+      @rowclick="onRowClick"
+    ></grid>
+    <!-- <table class="bg-gray-900 table w-full">
+      <tr class="border ..." v-for="exchange in exchange" :key="exchange.id">
+       <td><input type="checkbox" v-model="exchange.stat"></td>
       
-    </table>
-
+       <td>
+        {{exchange.id}}
+       </td>
+        <td>
+          {{exchange.name}}
+        </td>
+      </tr>
+    </table> -->
   </div>
-  {{exchange}}
+  <div>
+    <table class="bg-gray-900 table w-full">
+      <tr class="border ..." v-for="exchlist in exchlist" :key="exchlist.id" v-if="exchlist.id === exch.id">
+        <td class="border border-slate-700 ...">
+          <tc-stock-stat :stat="exchlist.status" />
+        </td>
+        <td class="text-white border border-slate-700 ...">
+          {{ exchlist.id }}
+        </td>
+        <td class="text-white border border-slate-700 ...">
+          {{ exchlist.name }}
+        </td>
+        <td class="text-white border border-slate-700 ...">
+          <tc-clock :serverDate="exchlist.serverDate1" :simpleTime="simpleTime" :glow="glow">
+          </tc-clock>
+        </td>
+      </tr>
+    </table>
+  </div>
+  {{ exchange }}
   <!-- {{exchlist}}
   {{exchange}} -->
 </template>
@@ -51,24 +51,20 @@ import TcStockStat from "../components/TcStockStat.vue";
 import { Grid } from "@progress/kendo-vue-grid";
 import "@progress/kendo-theme-default/dist/all.css";
 
-
 export default {
   components: {
-   
     TcClock,
     TcStockStat,
     Grid: Grid,
   },
 
-
   props: ["exchlist"],
   data() {
     return {
-
       // exc: this.exchange
-      glow: 'white  ',
-      simpleTime: true, 
-      exc:this.exchlist,
+      glow: "white  ",
+      simpleTime: true,
+      exc: this.exchlist,
       exch: [],
       huh: {},
       selectedField: "selected",
@@ -80,13 +76,13 @@ export default {
       ],
     };
   },
-   watch: {
-  exchlist:{
-    handler(){
-      this.refreshArraydata();
+  watch: {
+    exchlist: {
+      handler() {
+        this.refreshArraydata();
+      },
+      deep: true,
     },
-    deep: true
-  },
   },
 
   computed: {
@@ -94,32 +90,31 @@ export default {
       return [...this.staticColumns];
     },
     matchingIds() {
-      return this.exch.filter(obj1 =>
-        this.exchange.some(obj2 => obj1.id === obj2.id)
+      return this.exch.filter((obj1) =>
+        this.exchange.some((obj2) => obj1.id === obj2.id)
       );
     },
     newObject() {
       var obj = {};
       obj = Object.assign({}, ...this.matchingIds());
-      return  obj;
-    }
+      return obj;
+    },
   },
-//     watch: {
-// //     test() {
-// //     // const obj;
-// //     for (let key in this.exc) {
-// //   if (this.exchlist.Object.prototype.hasOwnProperty.call(key)) {
-// //     // If the property is not present in object2, delete it from object1
-// //     this.huh.push(this.exc.dataItem)
-// // }
-// // }
-// //   },
-//      },
+  //     watch: {
+  // //     test() {
+  // //     // const obj;
+  // //     for (let key in this.exc) {
+  // //   if (this.exchlist.Object.prototype.hasOwnProperty.call(key)) {
+  // //     // If the property is not present in object2, delete it from object1
+  // //     this.huh.push(this.exc.dataItem)
+  // // }
+  // // }
+  // //   },
+  //      },
   methods: {
-     refreshArraydata(){
-      this.exchange= []
-      this.exchange = this.exchlist;
-     },
+    refreshArraydata() {
+      
+    },
     setDateTime() {
       this.simpleTime = true;
     },
@@ -128,24 +123,15 @@ export default {
       event.dataItem[this.selectedField] = !event.dataItem[this.selectedField];
       if (event.dataItem[this.selectedField] == false) {
         this.exch.splice(this.exch.indexOf(event.dataItem), 1);
-      } else 
-      {
+      } else {
         this.exch.push(event.dataItem);
-        
-      
       }
     },
   },
   mounted() {
- 
- setInterval(() => {
-   
-   this.setDateTime();
- }, 1000);
-
-},
- 
- 
- 
+    setInterval(() => {
+      this.setDateTime();
+    }, 1000);
+  },
 };
 </script>
