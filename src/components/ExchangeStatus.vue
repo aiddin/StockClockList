58213e4639e4e9a1c901ff12d48fed1a4ce96d03
aiddin <arcticdessert@gmail.com>
2022-12-13 17:48:11 +0800

    exchange stat created

---
+++ src/components/ExchangeStatus.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <div>
+      <grid
+        ref="grid"
+        :style="{ height: '150px' }"
+        :data-items="exchlist"
+        :selected-field="selectedField"
+        :columns="columns"
+        :resizable="true"
+        @rowclick="onRowClick"
+      ></grid>
+    </div>
+  </div>
+</template>
+
+<script>
+import { Grid } from "@progress/kendo-vue-grid";
+
+import "@progress/kendo-theme-default/dist/all.css";
+export default {
+  components: {
+    Grid: Grid,
+  },
+  props: ["exchlist"],
+  data() {
+    return {
+      glow: "white",
+      simpleTime: true,
+      selectedID: 1,
+      exc: this.exchlist,
+      exch: ["MYX"],
+      selectedField: "selected",
+      exchange: this.exchlist,
+      staticColumns: [
+        { field: "id", title: "ID", width: "150px" },
+        { field: "name", title: "Exchange Name" },
+        { field: "status", title: "Exchange Status" },
+        { field: "serverDate", title: "Exchange Time" },
+      ],
+    };
+  },
+  computed: {
+    columns() {
+      return [...this.staticColumns];
+    },
+  },
+  methods: {
+    onRowClick(event) {
+      event.dataItem[this.selectedField] = !event.dataItem[this.selectedField];
+      this.exch = [];
+      this.exch.push(event.dataItem.id);
+    },
+  },
+};
+</script>
+<style></style>
--- src/components/TcStockExch.vue
+++ src/components/TcStockExch.vue
@@ -1,82 +1,64 @@
+
 <template>
+  <window><exchange-status :exchlist="exchlist"/></window>
   <div>
-    <grid
-      ref="grid"
-      :style="{ height: '150px' }"
-      :data-items="exchlist"
-      :selected-field="selectedField"
-      :columns="columns"
-      :resizable="true"
-      @rowclick="onRowClick"
-    ></grid>
-  </div>
-  <div>
-    <table class="table-fixed bg-gray-900 table w-full" v-for="exchlist in exchlist" :key="exchlist.id">
+    <table class="table-fixed bg-gray-900 table " v-for="exchlist in exchlist" :key="exchlist.id">
       <tr  v-if="filter(exchlist)" >
-       
         <td class="text-white auto" >
-          {{ exchlist.id }}
+          <h1 class="huh">{{ exchlist.id }}</h1> 
         </td>
         <td>
-          <h1 class="" > <tc-stock-stat :stat="exchlist.status" /></h1>
+          <h1 class="huh"><tc-stock-stat :stat="exchlist.status" /></h1>
         </td>
-     
         <td >
-          <tc-clock :serverDate="exchlist.serverDate" :simpleTime="simpleTime" :glow="glow">
+         <tc-clock :serverDate="exchlist.serverDate" :simpleTime="simpleTime" :glow="glow">
           </tc-clock>
         </td>
       </tr>
     </table>
   </div>
-  
+
  
 </template>
 <script>
 import TcClock from "../components/TcClock.vue";
 import TcStockStat from "../components/TcStockStat.vue";
-import { Grid } from "@progress/kendo-vue-grid";
-import "@progress/kendo-theme-default/dist/all.css";
-
+import ExchangeStatus from "../components/ExchangeStatus.vue";
 export default {
   components: {
     TcClock,
     TcStockStat,
-    Grid: Grid,
+    ExchangeStatus
+    // Grid: Grid,
   },
 
   props: ["exchlist"],
   data() {
     return {
-      glow: "yellow",
+      glow: "white",
       simpleTime: true,
       selectedID: 1,
       exc: this.exchlist,
-      exch: [],
+      exch: ['MYX'],
       selectedField: "selected",
       exchange: this.exchlist,
-      staticColumns: [
-        { field: "id", title: "ID", width: "150px" },
-        { field: "name", title: "Exchange Name" },
-        { field: "status", title: "Exchange Status" },
-        { field: "serverDate", title: "Exchange Time" },
-      ],
+      
     };
-  },
-  computed: {
-    columns() {
-      return [...this.staticColumns];
-    },
   },
   methods: {
     filter(exchlist) {
         return this.exch.find(id => id === exchlist.id);
     },
-    onRowClick(event) {
-      event.dataItem[this.selectedField] = !event.dataItem[this.selectedField];
-      this.exch =[];
-      this.exch.push(event.dataItem.id);
-    },
-  },
-  
+    
+  }, 
 };
 </script>
+<style>
+
+.huh{
+  transition: 0.2s;
+  font-size: 6vh;
+  margin: 0;
+  padding: 0;
+}
+</style>
